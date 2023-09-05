83eda7d9c8d3f534eaf1bdd4598928ce262f5e99
smeemarket <smeemarket@gmai.com>
2023-09-05 20:34:03 +0630

    to delete

--- app/Providers/Filament/CustomersPanelProvider.php
+++ app/Providers/Filament/CustomersPanelProvider.php
@@ -4,11 +4,10 @@ namespace App\Providers\Filament;
 
 use Filament\Pages;
 use Filament\Panel;
-use App\Models\Team;
 use Filament\Widgets;
 use Filament\PanelProvider;
-use App\Filament\Pages\Auth\Login;
-use App\Http\Middleware\Authenticate;
+use Filament\Support\Colors\Color;
+use Filament\Http\Middleware\Authenticate;
 use Jeffgreco13\FilamentBreezy\BreezyCore;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -20,33 +19,26 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 
-class AdminPanelProvider extends PanelProvider
+class CustomersPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->default()
-            ->id('admin')
-            ->path('admin')
-            ->login(Login::class) // Login::class ထည့်လျှင် checked Remember me
-            ->registration()
-            ->passwordReset()
-            ->emailVerification()
-            ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
-            ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
+            ->id('customers')
+            ->path('customers')
+            ->colors([
+                'primary' => Color::Amber,
+            ])
+            ->discoverResources(in: app_path('Filament/Customers/Resources'), for: 'App\\Filament\\Customers\\Resources')
+            ->discoverPages(in: app_path('Filament/Customers/Pages'), for: 'App\\Filament\\Customers\\Pages')
             ->pages([
                 Pages\Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            ->discoverWidgets(in: app_path('Filament/Customers/Widgets'), for: 'App\\Filament\\Customers\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
             ])
-            ->navigationGroups([
-                'Blog',
-                'Shop',
-            ])
-            ->databaseNotifications()
             ->middleware([
                 EncryptCookies::class,
                 AddQueuedCookiesToResponse::class,
@@ -61,9 +53,9 @@ class AdminPanelProvider extends PanelProvider
             ->authMiddleware([
                 Authenticate::class,
             ])
-            ->plugins([
-                \BezhanSalleh\FilamentShield\FilamentShieldPlugin::make(),
+            ->authGuard('customers')
+            ->plugin(
                 BreezyCore::make()
-            ]);
+            );
     }
 }
